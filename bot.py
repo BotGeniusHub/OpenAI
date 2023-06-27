a684af40ece8af1f7467f573a4a3096d06cb1af8
BotGeniusHub <136921156+BotGeniusHub@users.noreply.github.com>
2023-06-27 20:49:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import openai
 from pyrogram import Client, filters, idle
 
 # Set up OpenAI API
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Initialize the Telegram bot
 bot = Client("question_bot", api_id=16743442, api_hash="12bbd720f4097ba7713c5e40a11dfd2a", bot_token="6206599982:AAECqgU3Os4kTjSuS_Zz-zcM7JZHWdvbI10")
